7ca44b25c2cee7b4bace6d6a5065f57a517698b8
abhip75 <abhishekpwr944@gmail.com>
2024-01-04 15:32:10 +0530

    ui fixes

--- app/loading.tsx
+++ app/loading.tsx
@@ -17,7 +17,7 @@ function LoadingResults(){
             <div className="space-y-2 p-5">
                 {[...Array(10)].map((_, i) => (
                     <div key={i} className="flex space-x-2 mx-auto max-w-7xl">
-                        <Skeleton className="h-20w-20 md:h-44 md:w-44 rounded-lg"/>
+                        <Skeleton className="h-20 w-20 md:h-44 md:w-44 rounded-lg"/>
                         <Skeleton className="h-44 w-full rounded-lg"/>
                     </div>
                 ))}
