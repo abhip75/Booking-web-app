33fc08bda3ed1c878deca2ef34ff482f285960f4
abhip75 <abhishekpwr944@gmail.com>
2024-01-04 15:26:04 +0530

    ui fixes and implemented search

---
+++ data/trending.ts
@@ -0,0 +1,52 @@
+export const trending_data = [
+    {
+        id: 1,
+        src: "/images/kashmir.jpg",
+        title: "Kashmir",
+        location: "India",
+        description: "25 Deals"
+    },
+    {
+        id: 2,
+        src: "/images/dubai.jpg",
+        title: "Dubai",
+        location: "United Arab Emirates",
+        description: "15 Deals"
+    },
+    {
+        id: 3,
+        src: "/images/south-korea.jpg",
+        title: "South Korea",
+        location: "Asia",
+        description: "32 Deals"
+    },
+    {
+        id: 4,
+        src: "/images/london.jpg",
+        title: "London",
+        location: "United Kingdom",
+        description: "45 Deals"
+    },
+    {
+        id: 5,
+        src: "/images/singapore.jpg",
+        title: "Singapore",
+        location: "Asia",
+        description: "88 Deals"
+    },
+    {
+        id: 6,
+        src: "/images/new-york.jpg",
+        title: "New  York City",
+        location: "United States of America",
+        description: "92 Deals"
+    },
+    
+    {
+        id: 7,
+        src: "/images/qatar.jpg",
+        title: "Qatar",
+        location: "United Arab Emirates",
+        description: "95 Deals"
+    },
+];
